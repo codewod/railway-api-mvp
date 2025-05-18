db6dc0a41de4c4eb714f5bf24ec3f4298b6aa3e4
Ignacio Veas <iveasquinteros@gmail.com>
2025-05-18 12:41:18 -0400

    Cambio mensajes para entorno productivo

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("URI desde env:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB conectado ✅");
   } catch (error) {
--- index.js
+++ index.js
@@ -13,6 +13,17 @@ app.use(express.json());
 app.use("/registros", registrosRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`Servidor corriendo en http://localhost:${PORT}`)
-);
+const ENV = process.env.NODE_ENV || "development";
+
+app.get("/", (_req, res) => {
+  res.json({
+    status: "✅ API funcionando correctamente",
+    ambiente: process.env.NODE_ENV || "development",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ API levantada en modo "${ENV}"`);
+  console.log(`📡 Escuchando en puerto: ${PORT}`);
+});
